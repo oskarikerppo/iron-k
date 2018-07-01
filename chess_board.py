10ddffbd00b8f725acd8699393a7fb4a9422059d
Oskari Kerppo <oeoker@utu.fi>
2018-07-02 00:04:01 +0300

    Check whether position is check

--- chess_board.py
+++ chess_board.py
@@ -43,6 +43,7 @@ class Board:
 		#Returns list of all legal moves for the side who's turn it is
 		sideToMove = self.sideToMove
 		moves = []
+		legal_moves = []
 		print(sideToMove)
 		if sideToMove == "white":
 			#Pawns
@@ -79,15 +80,22 @@ class Board:
 								moves.append(self.pawns[j] + 'x' + self.pawns[j-1] + str(i+2) + 'e.p.')
 					if self.board[i][j] == 2: #White Knight:
 						knightMoves = []
-						print("White knight moves")
+						#print("White knight moves")
 						for l in (-2,-1,1,2):
 							for m in (-2,-1,1,2):
 								if abs(l) != abs(m) and 0 <= i + l <= 7 and 0 <= j + m <= 7:#Move is not to outside board
 									if self.board[i+l][j+m] == 0:#Not taking anything
 										knightMoves.append('N' + self.coordinates[i][j] + self.coordinates[i+l][j+m])
 									elif self.board[i+l][j+m]  < 0:#Taking pawn
-										knightMoves.append('N' + self.coordinates[i+l][j+m] + 'x' + self.coordinates[i+l][j+m])
+										knightMoves.append('N' + self.coordinates[i][j] + 'x' + self.coordinates[i+l][j+m])
 						moves = moves + knightMoves
+			for move in moves:
+				self.makeMove(move,False)
+				if "white" not in self.isCheck():
+					legal_moves.append(move)
+				else:
+					print move
+				self.undoMove()
 
 		else:#Black to move
 			#Pawns
@@ -125,7 +133,7 @@ class Board:
 								moves.append(self.pawns[j] + 'x' + self.pawns[j-1] + str(i) + 'e.p.')	
 					if self.board[i][j] == -2: #Black Knight:
 						knightMoves = []
-						print("Black knight moves")
+						#print("Black knight moves")
 						for l in (-2,-1,1,2):
 							for m in (-2,-1,1,2):
 								if abs(l) != abs(m) and 0 <= i + l <= 7 and 0 <= j + m <= 7:#Move is not to outside board
@@ -133,10 +141,17 @@ class Board:
 										knightMoves.append('N' + self.coordinates[i][j] + self.coordinates[i+l][j+m])
 									elif self.board[i+l][j+m]  > 0:#Taking pawn
 										knightMoves.append('N' + self.coordinates[i][j] + 'x' + self.coordinates[i+l][j+m])
-						moves = moves + knightMoves		
-		print(moves)
-		self.moves = moves
-		return moves
+						moves = moves + knightMoves	
+			for move in moves:
+				self.makeMove(move,False)
+				if "black" not in self.isCheck():
+					legal_moves.append(move)
+				else:
+					print move
+				self.undoMove()	
+		print(legal_moves)
+		self.moves = legal_moves
+		return legal_moves
 
 	def getBoardConfiguration(self, board):
 		return np.copy(board)
@@ -144,25 +159,25 @@ class Board:
 
 
 
-	def makeMove(self, move):
+	def makeMove(self, move,checkMoves=True):
 		self.boardHistory.append(self.getBoardConfiguration(self.board))
 		color = self.sideToMove
 		if color == 'white':
 			color = 1
 		else:
 			color = -1
-		if not self.moves:
+		if not self.moves and checkMoves:
 			self.moves = self.legalMoves()
-		if move not in self.moves:
+		if move not in self.moves and checkMoves:
 			print("Illegal move!")
 			return "Illegal move"
 		#Pawns
 		if move[:1] in self.pawns and 'x' not in move:
 			column = self.pawns.index(move[:1])
-			print(color)
-			print(column)
+			#print(color)
+			#print(column)
 			row = int(move[1:]) - 1
-			print(row)
+			#print(row)
 			self.board[row][column] = color
 			if self.board[row - color][column] == color:
 				self.board[row - color][column] = 0
@@ -170,7 +185,7 @@ class Board:
 				self.board[row - 2 * color][column] = 0
 		if move[:1] in self.pawns and 'x' in move:
 			target = move[2:4]
-			print(target)
+			#print(target)
 			target = zip(*np.where(self.coordinates == target))[0]
 			row = target[0]
 			column = target[1]
@@ -203,7 +218,7 @@ class Board:
 		if len(self.moveHistory) == 0:
 			print("No moves have been made!")
 			return 0
-		print(self.board == self.boardHistory[-1])
+		#print(self.board == self.boardHistory[-1])
 		self.board = self.boardHistory[-1]
 		self.boardHistory = self.boardHistory[:-1]
 		self.moveHistory = self.moveHistory[:-1]
@@ -217,11 +232,11 @@ class Board:
 		#Otherwise returns Empty list
 		#First check if white king is in check
 		checks = []
-		diagonalCheckGivers = [1,3,5,6]#Pawn,bishop,queen and king
+		diagonalCheckGivers = [3,5,6]#Bishop,queen and king
 		verticalCheckGivers = [4,5,6]#Rook, queen and king
 
 		whiteKing = zip(*np.where(self.board == 6))[0]
-		print whiteKing
+		#print whiteKing
 		kingColumn = self.board[:,whiteKing[1]]
 		kingRow = self.board[whiteKing[0]]
 		kingRightDiagonal = np.array([])
@@ -250,45 +265,150 @@ class Board:
 				kingLeftDiagonal = np.append(kingLeftDiagonal, self.board[row][column])
 			row += 1
 			column -= 1
-		print kingColumn
-		print kingRow
-		print kingRightDiagonal
-		print kingLeftDiagonal
+		#print kingColumn
+		#print kingRow
+		#print kingRightDiagonal
+		#print kingLeftDiagonal
+
 		for row in [kingColumn,kingRow]:
+			row = np.insert(row,0,0)
+			row = np.append(row,0)
 			kingIndex = np.where(row == 6)[0][0]
-			if 0 < kingIndex < 7:
-				if -1*row[kingIndex + 1] in verticalCheckGivers:
-					 checks.append("white")
-				if -1*row[kingIndex - 1] in verticalCheckGivers:
-				 	checks.append("white")
-		 	elif kingIndex == 0:
-		 		if -1*row[kingIndex + 1] in verticalCheckGivers:
-					checks.append("white")
-			else:
-				if -1*row[kingIndex - 1] in verticalCheckGivers:
-			 		checks.append("white")
+			try:
+				in_front = row[kingIndex + 1]
+			except:
+				in_front = 0
+			if -1*in_front in verticalCheckGivers:
+				print("check from front")
+				checks.append("white")
+			try:
+				in_back = row[kingIndex - 1]
+			except:
+				in_back = 0
+			if -1*in_back in verticalCheckGivers:
+				checks.append("white")
+			
 		for row in [kingLeftDiagonal,kingRightDiagonal]:
+			row = np.insert(row,0,0)
+			row = np.append(row,0)
 			kingIndex = np.where(row == 6)[0][0]
-			if 0 < kingIndex < 7:
-				if -1*row[kingIndex + 1] in diagonalCheckGivers:
-					 checks.append("white")
-				if -1*row[kingIndex - 1] in diagonalCheckGivers:
-				 	checks.append("white")
-		 	elif kingIndex == 0:
-		 		if -1*row[kingIndex + 1] in diagonalCheckGivers:
+			try:
+				in_front = row[kingIndex + 1]
+			except:
+				in_front = 0
+			if -1*in_front in verticalCheckGivers:
+				checks.append("white")
+			try:
+				in_back = row[kingIndex - 1]
+			except:
+				in_back = 0
+			if -1*in_back in verticalCheckGivers:
+				checks.append("white")
+			if whiteKing[0] < 7:
+				if self.board[whiteKing[0] + 1][whiteKing[1] + 1] == -1 or self.board[whiteKing[0] + 1][whiteKing[1] - 1] == -1:
 					checks.append("white")
-			else:
-				if -1*row[kingIndex - 1] in diagonalCheckGivers:
-			 		checks.append("white")
-		return False
+		
+		#Knights
+		for l in (-2,-1,1,2):
+			for m in (-2,-1,1,2):
+				if abs(l) != abs(m) and 0 <= whiteKing[0] + l <= 7 and 0 <= whiteKing[1] + m <= 7:
+					if self.board[whiteKing[0] + l][whiteKing[1] + m] == -2:
+						checks.append("white")
+		
+		blackKing = zip(*np.where(self.board == -6))[0]
+		#print blackKing
+		kingColumn = self.board[:,blackKing[1]]
+		kingRow = self.board[blackKing[0]]
+		kingRightDiagonal = np.array([])
+		row = blackKing[0]
+		column = blackKing[1]
+		while row >= 0 and column >= 0:
+			row -= 1
+			column -= 1
+		row += 1
+		column += 1
+		while row < 8 and column < 8:
+			if self.board[row][column] != 0:
+				kingRightDiagonal = np.append(kingRightDiagonal, self.board[row][column])
+			row += 1
+			column += 1
+		kingLeftDiagonal = np.array([])
+		row = blackKing[0]
+		column = blackKing[1]
+		while row >= 0 and column < 8:
+			row -= 1
+			column += 1
+		row += 1
+		column -= 1
+		while row < 8 and column >= 0:
+			if self.board[row][column] != 0:
+				kingLeftDiagonal = np.append(kingLeftDiagonal, self.board[row][column])
+			row += 1
+			column -= 1
+		#print kingColumn
+		#print kingRow
+		#print kingRightDiagonal
+		#print kingLeftDiagonal
+		for row in [kingColumn,kingRow]:
+			row = np.insert(row,0,0)
+			row = np.append(row,0)
+			kingIndex = np.where(row == -6)[0][0]
+			try:
+				in_front = row[kingIndex + 1]
+			except:
+				in_front = 0
+			if in_front in verticalCheckGivers:
+				checks.append("black")
+			try:
+				in_back = row[kingIndex - 1]
+			except:
+				in_back = 0
+			if in_back in verticalCheckGivers:
+				checks.append("black")
+			
+		for row in [kingLeftDiagonal,kingRightDiagonal]:
+			row = np.insert(row,0,0)
+			row = np.append(row,0)
+			kingIndex = np.where(row == -6)[0][0]
+			try:
+				in_front = row[kingIndex + 1]
+			except:
+				in_front = 0
+			if in_front in verticalCheckGivers:
+				checks.append("black")
+			try:
+				in_back = row[kingIndex - 1]
+			except:
+				in_back = 0
+			if in_back in verticalCheckGivers:
+				checks.append("black")
+			if blackKing[0] > 0:
+				if self.board[blackKing[0] - 1][blackKing[1] + 1] == 1 or self.board[blackKing[0] - 1][blackKing[1] - 1] == 1:
+					checks.append("black")
+			
+		#Knights
+		for l in (-2,-1,1,2):
+			for m in (-2,-1,1,2):
+				if abs(l) != abs(m) and 0 <= blackKing[0] + l <= 7 and 0 <= blackKing[1] + m <= 7:
+					if self.board[blackKing[0] + l][blackKing[1] + m] == 2:
+						checks.append("black")
+		print checks
+		return checks
 
 
 x = Board()
 x.showBoard()
 x.legalMoves()
-x.boardHistory
-'''
+#x.isCheck()
+x.makeMove('Nb1a3')
+x.makeMove('a6')
 
+x.makeMove('Na3b5')
+x.makeMove('a5')
+x.showBoard()
+x.isCheck()
+x.legalMoves()
+'''
 x.undoMove()
 x.makeMove('a4')
 x.showBoard()
