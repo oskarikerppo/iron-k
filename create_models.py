9a67ad15b46f6a88f4f5956630d143eaa266b375
Oskari Kerppo <oeoker@utu.fi>
2018-07-04 02:02:27 +0300

    First nerual networks

--- create_models.py
+++ create_models.py
@@ -17,7 +17,7 @@ W.add(Dense(256, activation='linear'))
 W.add(Dense(256, activation='linear'))	               
 W.add(Dense(1, activation='tanh'))
 #Compile model
-W.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+W.compile(loss=keras.losses.mean_squared_error, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
 W.summary()
 
 
@@ -32,4 +32,34 @@ state = np.array([[ 0.,  4.,  0.,  6.,  0.,  3.,  0.,  0.],
 state = state.ravel()
 state = np.reshape(state,(-1,64,1))
 
+state2 = np.array([[ 0.,  4.,  0.,  6.,  0.,  3.,  0.,  0.],
+       [ 0.,  3.,  0.,  0.,  1.,  0.,  4.,  0.],
+       [ 1.,  0.,  0.,  0.,  0.,  0.,  1.,  0.],
+       [ 0.,  0.,  1.,  1.,  0.,  1.,  0.,  0.],
+       [ 0., -1.,  1., -1.,  0., -1.,  2., -1.],
+       [ 0.,  0.,  0.,  0.,  0.,  2., -6., -4.],
+       [ 0.,  1.,  0.,  0.,  0.,  5., -1.,  0.],
+       [ 0.,  0.,  0.,  0.,  0., -3.,  0.,  0.]])
+state2 = state2.ravel()
+state2 = np.reshape(state2,(-1,64,1))
+
 print W.predict(state)
+W.save('Models\\walt.h5')
+
+
+B = Sequential()
+B.add(Conv1D(64, kernel_size=5,activation='linear',input_shape=(64,1),padding='same'))
+B.add(MaxPooling1D(pool_size=2,padding='same'))
+B.add(Conv1D(128, kernel_size=5,activation='linear',input_shape=(64,1),padding='same'))
+B.add(MaxPooling1D(pool_size=2,padding='same'))
+B.add(Flatten())
+B.add(Dense(256, activation='linear'))  
+B.add(Dense(256, activation='linear'))	               
+B.add(Dense(1, activation='tanh'))
+#Compile model
+B.compile(loss=keras.losses.mean_squared_error, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+B.summary()
+
+print W.predict(state2)
+
+B.save('Models\\bob.h5')
--- engine.py
+++ engine.py
@@ -1,37 +1,108 @@
 import chess_board as cb
 from random import randint
 from time import time
+import numpy as np
+import ast
+
+
+import keras
+from keras.models import Sequential, Input, Model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv1D, MaxPooling1D
+from keras.layers.normalization import BatchNormalization
+from keras.models import load_model
 
 
 
-beginning = time()
-board = cb.Board()
-board.newBoard()
-
-gameOver = False
-winner = ""
-start = time()
-while not gameOver:
-	white_moves = board.legalMoves()
-	board.makeMove(white_moves[randint(0,len(white_moves)-1)])
-	gameOver = board.gameOver()
-	if gameOver != False:
-		winner = gameOver
-		break
-	black_moves = board.legalMoves()
-	board.makeMove(black_moves[randint(0,len(black_moves)-1)])
-	gameOver = board.gameOver()
-	if gameOver != False:
-		winner = gameOver
-		break
-	print time() - start
-	start = time()
 
 
-move_history = board.moveHistory
-print move_history
-board_configuration = board.board
-#print board_configuration
-board.showBoard()
-print winner
-print start - beginning
+def main():
+	with open('Games\\game_library.txt', 'r+') as file:
+		games = file.read()
+	games = ast.literal_eval(games)
+	if not games.keys():
+		key = 1
+	else:
+		key = max(games.keys()) + 1
+	bob = load_model('Models\\bob.h5')
+	walt = load_model('Models\\walt.h5')
+
+	epochs = 6
+	for i in range(epochs):
+		beginning = time()
+		board = cb.Board()
+		board.newBoard()
+		gameOver = False
+		winner = ""
+		start = time()
+		while not gameOver:
+			white_moves = board.legalMoves()
+			white_scores = []
+			for move in white_moves:
+				board.makeMove(move)
+				white_scores.append(walt.predict(np.reshape(board.board,(-1,64,1))))
+				board.undoMove()
+			board.makeMove(white_moves[np.argmax(white_scores)])
+			gameOver = board.gameOver()
+			if gameOver != False:
+				winner = gameOver
+				break
+			black_moves = board.legalMoves()
+			black_scores = []
+			for move in black_moves:
+				board.makeMove(move)
+				black_scores.append(bob.predict(np.reshape(board.board,(-1,64,1))))
+				board.undoMove()
+			board.makeMove(black_moves[np.argmax(black_scores)])
+			gameOver = board.gameOver()
+			if gameOver != False:
+				winner = gameOver
+				break
+			print time() - start
+			start = time()
+
+		games[key] = board.moveHistory
+		key += 1
+
+		move_history = board.moveHistory
+		print move_history
+		board_configuration = board.board
+		#print board_configuration
+		board.showBoard()
+		print winner
+		print start - beginning
+		print "Game over! Training..."
+		if winner == "white":
+			walt_reward = 1.0
+			bob_reward = -1.0
+		elif winner == "black":
+			walt_reward = -1.0
+			bob_reward = 1.0		
+		else:
+			walt_reward = -0.2
+			bob_reward = 0.2
+		train_w_array = [x.ravel().reshape(-1,64,1) for x in board.boardHistory[::2]]
+		train_w = np.vstack(train_w_array)
+		train_w_rewards = np.array([])
+		for i in range(len(board.boardHistory[::2]), 0, -1):
+			train_w_rewards = np.append(train_w_rewards,walt_reward/i)
+		walt.fit(train_w,train_w_rewards, batch_size=1, epochs=1, verbose=0)
+
+		train_b_array = [x.ravel().reshape(-1,64,1) for x in board.boardHistory[1::2]]
+		train_b = np.vstack(train_b_array)
+		train_b_rewards = np.array([])
+		for i in range(len(board.boardHistory[1::2]), 0, -1):
+			train_b_rewards = np.append(train_b_rewards,bob_reward/i)
+		bob.fit(train_b,train_b_rewards, batch_size=1, epochs=1, verbose=0)
+
+
+
+
+	with open('Games\\game_library.txt', 'w+') as file:
+		file.write(str(games))
+
+	bob.save('Models\\bob.h5')
+	walt.save('Models\\walt.h5')
+
+if __name__ == "__main__":
+	main()
